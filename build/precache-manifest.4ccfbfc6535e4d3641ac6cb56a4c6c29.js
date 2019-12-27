3f010c25fbc5b37944664ed5666a602674a843db
guanpengchn <875499765@qq.com>
2019-12-27 09:29:45 +0800

    增加bulid

---
+++ build/precache-manifest.4ccfbfc6535e4d3641ac6cb56a4c6c29.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "cedf10ed534aa30ebd47474a1c96507f",
+    "url": "/static/media/day.cedf10ed.svg"
+  },
+  {
+    "revision": "1ba7d444618c9415b208",
+    "url": "/static/css/main.54e1feea.chunk.css"
+  },
+  {
+    "revision": "910bd05cddf498327bcf597c3a1d5280",
+    "url": "/static/media/thanks.910bd05c.svg"
+  },
+  {
+    "revision": "f72338a66005f1f38cf7",
+    "url": "/static/js/1.f72338a6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a05482ff6f146525a696695daec8e274",
+    "url": "/static/media/githubWhite.a05482ff.svg"
+  },
+  {
+    "revision": "0a0da2839322114f25a21f1ffbfd88ef",
+    "url": "/static/media/github.0a0da283.svg"
+  },
+  {
+    "revision": "246d6981c89c7fb4a5fbc4365e8a723d",
+    "url": "/static/media/repo.246d6981.svg"
+  },
+  {
+    "revision": "a184c99ba3ea968f957407c35e0b5c21",
+    "url": "/static/media/line.a184c99b.svg"
+  },
+  {
+    "revision": "1ba7d444618c9415b208",
+    "url": "/static/js/main.1ba7d444.chunk.js"
+  },
+  {
+    "revision": "241a4cd2b3fdbec07509be94120f51ce",
+    "url": "/static/media/moon.241a4cd2.svg"
+  },
+  {
+    "revision": "07397a2f4330ee2254b609eaf7afa7c2",
+    "url": "/static/media/focus.07397a2f.svg"
+  },
+  {
+    "revision": "1d9f8f501718748899c5f2035899c0ff",
+    "url": "/static/media/time.1d9f8f50.svg"
+  },
+  {
+    "revision": "b67afa0bf9df4f70abac833e81ae1a95",
+    "url": "/static/media/talk.b67afa0b.svg"
+  },
+  {
+    "revision": "d1d20e527b4f1203a360f1f02f47f0ca",
+    "url": "/static/media/language.d1d20e52.svg"
+  },
+  {
+    "revision": "4bdf00c2bc435710081d7857e924577a",
+    "url": "/static/media/prize.4bdf00c2.svg"
+  },
+  {
+    "revision": "f72338a66005f1f38cf7",
+    "url": "/static/css/1.bbc83574.chunk.css"
+  },
+  {
+    "revision": "404018a7e5d9057c9022e390869df203",
+    "url": "/index.html"
+  }
+];
